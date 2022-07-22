6629fa9cac08842a310c91ddd163fe6e1e4a842a
harutiro <harutiro2027@outlook.jp>
2022-07-22 21:28:29 +0900

    empty

--- myqrencode.rb
+++ myqrencode.rb
@@ -14,17 +14,4 @@ class Myqrencode < Formula
     system "make"
     bin.install "chikubeam"
   end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test myqrencode`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end
